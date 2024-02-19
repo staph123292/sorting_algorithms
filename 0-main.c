f2e6c5a01302c3aa7586883a3c4bbf00fe09f180
Mustapha KHALID <mosstaf096@gmail.com>
2024-02-19 03:15:27 -0800

    done

--- 0-main.c
+++ 0-main.c
@@ -14,7 +14,7 @@ int main(void)
 
     print_array(array, n);
     printf("\n");
-    bubble_sorting(array, n);
+    bubble_sort(array, n);
     printf("\n");
     print_array(array, n);
     return (0);
