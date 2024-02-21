32715d2c7b5dac2c87a95d01d5d8f5901bca9f16
Mustapha KHALID <mosstaf096@gmail.com>
2024-02-20 18:42:28 -0800

    done

--- sort.h
+++ sort.h
@@ -14,21 +14,20 @@
 
 typedef enum boolean_n
 {
-    false = 0,
-    true
+	false = 0,
+	true
 } boolean_n;
 /**
  * struct listint_s - Doubly linked list node
- * 
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
 */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
